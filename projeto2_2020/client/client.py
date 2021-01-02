178459e9dca8853bb0b6d7bef342643f6b4a9648
juanlessa <juanvlessa@ua.pt>
2021-01-02 21:35:09 +0000

    merge auth and cryptohphy

--- projeto2_2020/client/client.py
+++ projeto2_2020/client/client.py
@@ -6,6 +6,21 @@ import os
 import subprocess
 import time
 import sys
+import random
+
+sys.path.append(os.path.abspath('../cryptography'))
+import diffiehellman
+import symmetriccrypt
+
+sys.path.append(os.path.abspath('../utils'))
+import utils
+import rsa_utils
+import PyKCS11
+import base64
+from cryptography.x509 import ObjectIdentifier
+from cryptography.hazmat.primitives import serialization
+
+
 
 logger = logging.getLogger('root')
 FORMAT = "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
@@ -13,6 +28,148 @@ logging.basicConfig(format=FORMAT)
 logger.setLevel(logging.INFO)
 
 SERVER_URL = 'http://127.0.0.1:8080'
+CLIENT_OID = ''
+
+def cryptography():
+    #diffie-hellman 
+    #get dh parameters
+    req = requests.get(f'{SERVER_URL}/api/dh-parameters')
+    if req.status_code == 200:
+        print("Got dh-parameters")
+    dh_parameters = req.json()
+    #generate private key
+    dh_private_key = diffiehellman.dh_generate_private_key(dh_parameters)
+    #public number y
+    dh_public_number_y = diffiehellman.dh_generate_public_key(dh_private_key)
+    
+    #get server public number
+    req = requests.post(f'{SERVER_URL}/api/dh-handshake', data=json.dumps([dh_public_number_y]).encode('latin'))   
+    if req.status_code == 200:
+        print("Got server public number")
+    server_public_number_y = req.json()[0]
+    
+    #calculate secret key to encrypt comunication
+    secret_key = diffiehellman.dh_calculete_common_secret(dh_private_key,server_public_number_y)
+    print("Calculated secret key to encrypt comunication")
+    
+    #Negotiate a cipher suite
+    req = requests.get(f'{SERVER_URL}/api/cipher-suite')
+    if req.status_code == 200:
+        print("Got ciphers list")
+    cipher_list = req.json()
+    #chose cipher to be used
+    cipher_list[0] = random.choice(cipher_list[0])  # Algorithm , 
+    if cipher_list[0] == 'ChaCha20':
+        cipher_list[1] = None
+    else:
+        cipher_list[1] = random.choice(cipher_list[1])  # Cipher Mode
+    cipher_list[2] = random.choice(cipher_list[2])  # Hash Functions
+    print(f"chosen ciphers:\n\tAlgorithm: {cipher_list[0]}\n\tCipher Mode: {cipher_list[1]}\n\tHash Function: {cipher_list[2]}")
+    #comunicate chosen ciphers to server
+    req = requests.post(f'{SERVER_URL}/api/chosen-ciphers', data=json.dumps(cipher_list).encode('latin'))   
+    if req.status_code == 200:
+        print("Got server encrypted message")
+    message = req.json()
+    message = message["data"].encode('latin')
+    #decrypt message
+    message = symmetriccrypt.decrypt(secret_key,message,cipher_list[0], cipher_list[1])
+    print(f"server message:  {message}")
+
+    return dh_parameters, dh_private_key, secret_key, cipher_list
+
+
+
+def auth():
+    global CLIENT_OID
+    client_nonce = os.urandom(64)
+    
+    req = requests.post(f'{SERVER_URL}/api/server_auth', data=json.dumps({"nonce":base64.b64encode(client_nonce).decode()}).encode())
+    if req.status_code == 200:
+       logger.debug(f"Got Server Certs and Signed Nonce")
+    
+    
+    #validate server certs
+    data = req.json()
+
+    server_nonce = base64.b64decode(data["server_nonce"].encode())
+
+    server_certificate = utils.certificate_object_from_pem(base64.b64decode(data["server_certificate"].encode()))
+    signed_client_nonce = base64.b64decode(data["signed_client_nonce"].encode())
+
+    cert_data = utils.load_cert_from_disk("../server_ca/SIOServerCA.pem")
+    cert = utils.certificate_object_from_pem(cert_data)
+
+    certificates={}
+    certificates[cert.subject.rfc4514_string()] = cert
+
+    chain=[]
+    chain_completed = utils.construct_certificate_chain(chain, server_certificate, certificates)
+    
+    if not chain_completed:
+        logger.debug(f"Couldn't complete the certificate chain")
+        return False
+        
+    else:
+        valid_chain, error_messages = utils.validate_certificate_chain(chain)
+
+        if not valid_chain:
+            logger.debug(error_messages)
+            return False  
+        else:
+            if not utils.verify_signature(server_certificate, signed_client_nonce, client_nonce):
+                return False
+    
+    #send cc info
+    session_success, session_data = utils.cc_session()
+
+    if not session_success:
+        logger.debug(f"Error establishing a new citizen card session: {session_data}")
+        return False
+
+    client_cert = utils.certificate_cc(session_data)
+    
+    client_certs = {}
+    client_certs["client_cc_certificate"] = base64.b64encode(client_cert).decode()
+    client_certs["signed_server_nonce"] = base64.b64encode(utils.sign_nonce_cc(session_data, server_nonce)).decode()
+    
+
+    #finalize auth
+    req = requests.post(f'{SERVER_URL}/api/client_auth', data=json.dumps(client_certs).encode())
+    if req.status_code == 200:
+       logger.debug(f"Server finished cc certificatcion chain")
+    
+    data = req.json()
+    if data["status"]:
+        logger.debug(f"Sucessfully authenticated CC")
+        oid = ObjectIdentifier("2.5.4.5")
+        CLIENT_OID = utils.certificate_object(client_cert).subject.get_attributes_for_oid(oid)[0].value
+        return True
+    else:
+        logger.debug(f"Could not authenticated CC")
+        return False
+
+    
+def rsa_exchange():
+    private_key, public_key = rsa_utils.generate_rsa_key_pair(2048, "../client_rsa_keys/client_rsa_key")
+
+    req = requests.post(f'{SERVER_URL}/api/rsa_exchange', 
+            data=json.dumps({
+                "client_rsa_pub_key":public_key.public_bytes(encoding=serialization.Encoding.PEM,
+                                      format=serialization.PublicFormat.SubjectPublicKeyInfo).decode()
+            }).encode()
+        )
+
+    if req.status_code == 200:
+       logger.debug(f"Received Server public rsa Key ")
+
+    data = req.json()
+
+    server_rsa_pub_key = data["server_rsa_pub_key"].encode()
+
+    fileToSave_public_key = open("../client_rsa_keys/server_rsa_pub_key.pub", 'wb')
+    fileToSave_public_key.write(server_rsa_pub_key)
+    fileToSave_public_key.close()
+
 
 def main():
     print("|--------------------------------------|")
@@ -23,10 +180,17 @@ def main():
     print("Contacting Server")
     
     # TODO: Secure the session
+    headers = {"oid":CLIENT_OID}
 
-    req = requests.get(f'{SERVER_URL}/api/list')
+    request = requests.Session()
+    request.headers.update(headers)
+    
+    req = request.get(f'{SERVER_URL}/api/list')
     if req.status_code == 200:
         print("Got Server List")
+    else:
+        print(req.json())
+        sys.exit(0)
 
     media_list = req.json()
 
@@ -64,18 +228,40 @@ def main():
 
     # Get data from server and send it to the ffplay stdin through a pipe
     for chunk in range(media_item['chunks'] + 1):
-        req = requests.get(f'{SERVER_URL}/api/download?id={media_item["id"]}&chunk={chunk}')
+        req = request.get(f'{SERVER_URL}/api/download?id={media_item["id"]}&chunk={chunk}')
         chunk = req.json()
        
         # TODO: Process chunk
-
         data = binascii.a2b_base64(chunk['data'].encode('latin'))
+        data_signature = chunk['data_signature'].encode('latin')
+
+        if not rsa_utils.rsa_verify(rsa_utils.load_rsa_public_key("../client_rsa_keys/server_rsa_pub_key.pub"), data, data_signature):
+            print("The file sent from the server is not of trust")
+            sys.exit(0)
+        
         try:
             proc.stdin.write(data)
         except:
             break
 
 if __name__ == '__main__':
-    while True:
-        main()
-        time.sleep(1)
+    
+    #stablish cryptography with server 
+    dh_parameters, dh_private_key, secret_key, cipher_list = cryptography()
+    #auth
+    auth_result = auth()
+    if auth_result:
+        while True:
+            rsa_exchange()
+            main()
+            time.sleep(1)
+        # try:
+        #     auth()
+        #     main()
+        #     time.sleep(1)
+        # except Exception as e :
+        #     print(e)
+        #     sys.exit(0)
+
+        
+        
--- projeto2_2020/server/server.py
+++ projeto2_2020/server/server.py
@@ -7,6 +7,20 @@ import binascii
 import json
 import os
 import math
+import sys
+
+sys.path.append(os.path.abspath('../cryptography'))
+import symmetriccrypt
+import diffiehellman
+
+sys.path.append(os.path.abspath('../utils'))
+import utils
+import rsa_utils
+import base64
+from cryptography.x509 import ObjectIdentifier
+from cryptography.hazmat.primitives import serialization
+
+
 
 logger = logging.getLogger('root')
 FORMAT = "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
@@ -30,13 +44,22 @@ CHUNK_SIZE = 1024 * 4
 class MediaServer(resource.Resource):
     isLeaf = True
 
+    def __init__(self):
+        self.server_nonce = None
+        self.authorized_users = []
+        #cryptography informations
+        self.client_chosen_ciphers = None
+        self.dh_parameters = None
+        self.dh_private_key = None
+        self.secret_key = None
+
     # Send the list of media files to clients
     def do_list(self, request):
 
-        #auth = request.getHeader('Authorization')
-        #if not auth:
-        #    request.setResponseCode(401)
-        #    return 'Not authorized'
+        oid = request.getHeader('oid')
+        if oid not in self.authorized_users:
+           request.setResponseCode(401)
+           return json.dumps('Not authorized').encode()
 
 
         # Build list
@@ -58,6 +81,12 @@ class MediaServer(resource.Resource):
 
     # Send a media chunk to the client
     def do_download(self, request):
+
+        oid = request.getHeader('oid')
+        if oid not in self.authorized_users:
+           request.setResponseCode(401)
+           return json.dumps('Not authorized').encode()
+
         logger.debug(f'Download: args: {request.args}')
         
         media_id = request.args.get(b'id', [None])[0]
@@ -105,18 +134,102 @@ class MediaServer(resource.Resource):
             f.seek(offset)
             data = f.read(CHUNK_SIZE)
 
+            data_signature = rsa_utils.rsa_sign(rsa_utils.load_rsa_private_key("../server_rsa_keys/server_rsa_key"), data)
+
             request.responseHeaders.addRawHeader(b"content-type", b"application/json")
             return json.dumps(
                     {
                         'media_id': media_id, 
                         'chunk': chunk_id, 
-                        'data': binascii.b2a_base64(data).decode('latin').strip()
+                        'data': binascii.b2a_base64(data).decode('latin').strip(),
+                        'data_signature': data_signature.decode('latin')
                     },indent=4
                 ).encode('latin')
 
         # File was not open?
         request.responseHeaders.addRawHeader(b"content-type", b"application/json")
         return json.dumps({'error': 'unknown'}, indent=4).encode('latin')
+
+    def server_authenticate(self, request):
+        dict = request.content.read()
+        data = json.loads(dict)
+
+        client_nonce = base64.b64decode(data["nonce"].encode())
+        
+        private_key = utils.load_private_key_file("../server_pk/SIOServerCertKey.pem")
+        signed_client_nonce = utils.sign_with_pk(private_key, client_nonce)
+        certificate = utils.load_cert_from_disk("../server_cert/SIOServerCert.pem")
+
+        self.server_nonce = os.urandom(64)
+
+        return json.dumps({
+                "server_certificate": base64.b64encode(certificate).decode(),
+                "signed_client_nonce": base64.b64encode(signed_client_nonce).decode(),
+                "server_nonce": base64.b64encode(self.server_nonce).decode()
+            }).encode('latin')
+
+    def client_authenticate(self, request):
+        dict = request.content.read()
+        data = json.loads(dict)
+
+        signed_server_nonce = base64.b64decode(data["signed_server_nonce"].encode())
+        
+        client_cc_certificate = utils.certificate_object(base64.b64decode(data["client_cc_certificate"].encode()))
+
+
+        path = "../cc_certificates"
+        certificates={}
+        
+        for filename in os.listdir(path):
+            if filename.endswith(".pem"): 
+                cert_data = utils.load_cert_from_disk(os.path.join(path, filename))
+                cert = utils.certificate_object_from_pem(cert_data)
+                certificates[cert.subject.rfc4514_string()] = cert
+        
+        chain = []
+        chain_completed = utils.construct_certificate_chain(chain, client_cc_certificate, certificates)
+
+        if not chain_completed:
+            logger.debug(f"Couldn't complete the certificate chain")
+            status = False
+
+        else:
+            valid_chain, error_messages = utils.validate_certificate_chain(chain)
+
+            if not valid_chain:
+                logger.error(error_messages)
+                status = False
+            else:
+                status = utils.verify_signature(client_cc_certificate, signed_server_nonce, self.server_nonce)
+
+        if status:
+            oid = ObjectIdentifier("2.5.4.5")
+            self.authorized_users.append(client_cc_certificate.subject.get_attributes_for_oid(oid)[0].value)
+
+            logger.debug(f"User logged in with success")
+            
+
+        return json.dumps({
+                "status":status
+            }).encode('latin')
+
+    def rsa_exchange(self, request):
+        private_key, public_key = rsa_utils.generate_rsa_key_pair(2048, "../server_rsa_keys/server_rsa_key")
+
+        dict = request.content.read()
+        data = json.loads(dict)
+
+        client_rsa_pub_key = data["client_rsa_pub_key"].encode()
+
+        fileToSave_public_key = open("../server_rsa_keys/client_rsa_pub_key.pub", 'wb')
+        fileToSave_public_key.write(client_rsa_pub_key)
+        fileToSave_public_key.close()
+
+        return json.dumps({
+                "server_rsa_pub_key":public_key.public_bytes(encoding=serialization.Encoding.PEM,
+                                      format=serialization.PublicFormat.SubjectPublicKeyInfo).decode()
+            }).encode('latin')
+            
 
     # Handle a GET request
     def render_GET(self, request):
@@ -127,11 +240,27 @@ class MediaServer(resource.Resource):
                 return self.do_get_protocols(request)
             #elif request.uri == 'api/key':
             #...
-            #elif request.uri == 'api/auth':
+            
+            #cryptography methods
+            elif request.path == b'/api/cipher-suite':
+                # Return list to client
+                # suported algorithms, cipher modes and hash functions
+                cipherModes = ["ECB", "CBF","CBC", "OFB"]
+                cipherAlgorithms = ['3DES','AES-128','ChaCha20']
+                hashFunctions = ["SHA-256", "SHA-384", "SHA-512", "MD5", "BLAKE-2"]
+                #ciphers
+                ciphers_list = [cipherAlgorithms, cipherModes, hashFunctions]
+                request.responseHeaders.addRawHeader(b"content-type", b"application/json")
+                return json.dumps(ciphers_list, indent=4).encode('latin')
+            elif request.path == b'/api/dh-parameters':
+                dh_parameters = diffiehellman.dh_generate_parameters()
+                self.dh_parameters = dh_parameters
+                request.responseHeaders.addRawHeader(b"content-type", b"application/json")
+                return json.dumps(dh_parameters, indent=4).encode('latin')
+
 
             elif request.path == b'/api/list':
                 return self.do_list(request)
-
             elif request.path == b'/api/download':
                 return self.do_download(request)
             else:
@@ -147,8 +276,49 @@ class MediaServer(resource.Resource):
     # Handle a POST request
     def render_POST(self, request):
         logger.debug(f'Received POST for {request.uri}')
-        request.setResponseCode(501)
-        return b''
+        try:
+            if request.uri == b'/api/server_auth':
+                return self.server_authenticate(request)
+            elif request.uri == b'/api/client_auth':
+                return self.client_authenticate(request)
+            elif request.uri == b'/api/rsa_exchange':
+                return self.rsa_exchange(request)
+            
+            #cryptography methods
+            if request.path == b'/api/dh-handshake':
+                #client public key (public number y)
+                client_public_number_y = json.loads(request.content.read())[0]
+                #generate my private key
+                self.dh_private_key = diffiehellman.dh_generate_private_key(self.dh_parameters)
+                #my public key (public number y)
+                dh_public_number_y = diffiehellman.dh_generate_public_key(self.dh_private_key)
+                #calculete secret key to be used to encrypt comunication
+                self.secret_key = diffiehellman.dh_calculete_common_secret(self.dh_private_key, client_public_number_y)
+                request.responseHeaders.addRawHeader(b"content-type", b"application/json")
+                return json.dumps([dh_public_number_y], indent=4).encode('latin')
+            elif request.path == b'/api/chosen-ciphers':
+                #client public key (public number y)
+                self.client_chosen_ciphers = json.loads(request.content.read())
+                print(self.client_chosen_ciphers)
+
+                message = "cryptography pattern established"
+                message = symmetriccrypt.encrypt(self.secret_key,message,self.client_chosen_ciphers[0],self.client_chosen_ciphers[1])
+                print(message)
+                message = message.decode('latin')
+                print(message)
+                print(type(message))
+                request.responseHeaders.addRawHeader(b"content-type", b"application/json")
+                return json.dumps({"data":message}, indent=4).encode('latin')
+
+            else:
+                request.responseHeaders.addRawHeader(b"content-type", b'text/plain')
+                return b'Methods: /api/protocols /api/list /api/download'
+
+        except Exception as e:
+            logger.exception(e)
+            request.setResponseCode(500)
+            request.responseHeaders.addRawHeader(b"content-type", b"text/plain")
+            return b''
 
 
 print("Server started")
